a7dcdc12dd6c4ae1254e01ede40b1efb5e18d478
S R <awdenton@gmail.com>
2021-09-15 07:34:48 -0400

    first push in a while

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTransition, animation, animated } from 'react-spring';
 import _ from 'lodash';
 import { GameCard } from '../components';
 import { data } from '../utils';
@@ -9,15 +10,20 @@ export default function Home(props) {
     const [discardPile, setDiscardPile] = useState([]);
     const [board1, setBoard1] = useState([]);
     const [board2, setBoard2] = useState([]);
-
     const [isPlayer1Turn, setIsPlayer1Turn] = useState(true);
+
+    const cardTransition = useTransition(drawPile, {
+        from: { x: -250, y: -50 },
+        enter: { x: 0, y: 0 },
+        leave: { x: 250, y: 50 }
+    });
 
     const newDeck = () => {
         let freshDeck = [];
 
         _.forEach(data.cardSuits, suit => {
             _.forEach(data.cardVals, val => {
-                freshDeck.push({ id: freshDeck.length + 1, cVal: val, cSuit: suit, flipped: false, canFlip: false });
+                freshDeck.push({ id: freshDeck.length + 1, cVal: val, cSuit: suit, flipped: false, canFlip: false, matched: null, backVisible: "visible", frontVisible: "visible" });
             });
         });
 
@@ -34,36 +40,70 @@ export default function Home(props) {
             newBoard2.push(deck.pop());
         }
 
-        setDrawPile(deck);
         setDiscardPile([]);
         setBoard1(newBoard1);
         setBoard2(newBoard2);
+        setDrawPile(deck);
     }
 
     const flipAllCards = () => {
-        let newBoard1 = _.slice(board1);
-        let newBoard2 = _.slice(board2);
-
-        _.forEach(newBoard1, card => {
-            card.flipped = !card.flipped
+        let board1Copy = _.map(board1, c => {
+            c.flipped = !c.flipped;
+            return c;
         });
-        _.forEach(newBoard2, card => {
-            card.flipped = !card.flipped
-        });
+        setBoard1(board1Copy);
 
-        setBoard1(newBoard1);
-        setBoard2(newBoard2);
+        let board2Copy = _.map(board2, c => {
+            c.flipped = !c.flipped;
+            return c;
+        });
+        setBoard2(board2Copy);
     }
 
+    const reset = () => {
+        setDiscardPile([]);
+        setBoard1([]);
+        setBoard2([]);
+        setDrawPile([]);
+    }
 
+    const draw = () => {
+        let drawCopy = _.slice(drawPile);
+        drawCopy[drawCopy.length - 1].flipped = !drawCopy[drawCopy.length - 1].flipped;
+        setDrawPile(drawCopy);
+        setTimeout(checkMatch, 2000);
+    }
+
+    const checkMatch = () => {
+        // if(drawPile[drawPile.length-1].cVal > 10) {
+            discard();
+        // }
+
+        // let activeBoard = _.slice(isPlayer1Turn ? board1 : board2);
+
+
+    }
+
+    const discard = () => {
+        let drawCopy = _.slice(drawPile);
+        drawCopy[drawCopy.length-1].flipped = !drawCopy[drawCopy.length-1].flipped
+        setDrawPile(drawCopy);
+        
+        let discardCopy = _.slice(discardPile);
+        discardCopy.push(drawCopy.pop());        
+        setDiscardPile(discardCopy);
+    }
 
     return (
-        <div className="game-board">
+        <div className="main">
             <button onClick={dealGame}>Deal</button>
             <button onClick={flipAllCards}>Flip</button>
+            <button onClick={reset}>Reset</button>
+
+            <h1>Player {isPlayer1Turn ? "1" : "2"}'s turn!</h1>
 
 
-            <div className="player-board player-one">
+            <div className="player board">
                 {
                     _.map(board1, (card, index) => {
                         return (
@@ -73,27 +113,34 @@ export default function Home(props) {
                 }
             </div>
 
-            <div className="draw-pile">
-                {
-                    _.map(drawDeck, (card, index) => {
-                        return (
-                            <GameCard cardInfo={card} key={index} />
-                        )
-                    })
-                }
+
+            <div className="util board">
+                <div className="pile" onClick={draw}>
+                    {cardTransition((style, item) => {
+                        return item ? <animated.div className="card-anim" style={style}><GameCard cardInfo={item}/></animated.div> : ""
+                    })}
+
+                    {/* {
+                        _.map(drawPile, (card, index) => {
+                            return (
+                                <GameCard cardInfo={card} key={index} />
+                            )
+                        })
+                    } */}
+                </div>
+
+                <div className="pile">
+                    {
+                        _.map(discardPile, (card, index) => {
+                            return (
+                                <GameCard cardInfo={card} key={index} />
+                            )
+                        })
+                    }
+                </div>
             </div>
 
-            <div className="discard-pile">
-                {
-                    _.map(board1, (card, index) => {
-                        return (
-                            <GameCard cardInfo={card} key={index} />
-                        )
-                    })
-                }
-            </div>
-
-            <div className="player-board player-two">
+            <div className="player board">
                 {
                     _.map(board2, (card, index) => {
                         return (
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import { data } from '../utils';
 
@@ -6,7 +6,7 @@ export default function GameCard(props) {
 
     const flipAnimation = useSpring({
         transform: `rotateY(${props.cardInfo.flipped ? 180 : 0}deg)`,
-        config: { mass: 1, tension: 250, friction: 35 }
+        config: { mass: 5, tension: 500, friction: 80 }
     });
 
     const getBackgroundOffset = id => {
@@ -15,15 +15,13 @@ export default function GameCard(props) {
         const hOff = (id % 13 !== 0 ? (id % 13) * -1 : -13);
         const vOff = Math.floor(id / 13) * -1;
 
-        return {
-            backgroundPosition: `${hOff * data.cardWidth}px ${vOff * cardHeight}px`,
-        }
+        return `${hOff * data.cardWidth}px ${vOff * cardHeight}px`;
     }
 
     return (
-        <animated.div className={`game-card`} style={flipAnimation} id={props.cardInfo.id}>
-            <div className="game-card-front" style={getBackgroundOffset(props.cardInfo.id)}></div>
-            <div className={`game-card-back`}></div>
-        </animated.div>
+        <div className="game-card" >
+            <animated.div className="game-card-front" style={{ visibility: props.cardInfo.frontVisible, backgroundPosition: getBackgroundOffset(props.cardInfo.id), rotateY: "180deg", ...flipAnimation }} />
+            <animated.div className="game-card-back" style={{visibility: props.cardInfo.backVisible, ...flipAnimation}} />
+        </div>
     );
 }
